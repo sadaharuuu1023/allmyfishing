534dd26230e6a9ffc76df9d0bbd6d44927103593
sadaharuuu1023 <sadaharuuu.1023@gmail.com>
2023-08-16 16:49:28 +0900

    投稿機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,36 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
   
   def index
     @tweet = Tweet.all
   end
+  
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end
 
 
 private
 
   def tweet_params
-    params.require(:tweet).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:title, :text_field, :text_tackle, :image).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = tweet.find(params[:id])
   end
 
 def move_to_index
